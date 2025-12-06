decdb4772211f7950848f79ddb3e0bc4766c7afe
priyanka <priah1709@gmail.com>
2025-12-06 12:13:19 +0530

    cloud

--- rfp_frontend/src/components/ProposalCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function ProposalCard({ proposal }) {
-  return (
-    <div className="border p-4 rounded shadow mb-4">
-      <h2 className="font-bold">{proposal.vendor_name}</h2>
-      <p>{proposal.proposal_details}</p>
-      <p>Price: ${proposal.price}</p>
-    </div>
-  );
-}
